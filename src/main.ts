4474f1289a6b59105b929c27cce4aa16dddc1914
joonamo <joonamo@users.noreply.github.com>
2024-06-30 17:38:20 +0300

    Use textContent instead of innerHTML

--- src/main.ts
+++ src/main.ts
@@ -14,25 +14,25 @@ const reset20Button = document.getElementById("reset20")
 
 const add1 = () => {
   score1 += 1
-  score1Container!.innerHTML = score1.toString()
+  score1Container!.textContent = score1.toString()
 }
 score1Add!.onclick = add1
 
 const sub1 = () => {
   score1 -= 1
-  score1Container!.innerHTML = score1.toString()
+  score1Container!.textContent = score1.toString()
 }
 score1Sub!.onclick = sub1
 
 const add2 = () => {
   score2 += 1
-  score2Container!.innerHTML = score2.toString()
+  score2Container!.textContent = score2.toString()
 }
 score2Add!.onclick = add2
 
 const sub2 = () => {
   score2 -= 1
-  score2Container!.innerHTML = score2.toString()
+  score2Container!.textContent = score2.toString()
 }
 score2Sub!.onclick = sub2
 
@@ -40,8 +40,8 @@ const reset0 = () => {
   score1 = 0
   score2 = 0
 
-  score1Container!.innerHTML = score1.toString()
-  score2Container!.innerHTML = score2.toString()
+  score1Container!.textContent = score1.toString()
+  score2Container!.textContent = score2.toString()
 }
 reset0Button!.onclick = reset0
 
@@ -49,8 +49,8 @@ const reset20 = () => {
   score1 = 20
   score2 = 20
 
-  score1Container!.innerHTML = score1.toString()
-  score2Container!.innerHTML = score2.toString()
+  score1Container!.textContent = score1.toString()
+  score2Container!.textContent = score2.toString()
 }
 reset20Button!.onclick = reset20
 
